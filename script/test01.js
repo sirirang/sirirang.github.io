b4e1cdeaf122f61b73f4e4ffbde3def20d4df03e
siri <ysh7731@gmail.com>
2018-08-15 20:45:45 +0900

    javascript 입문 - 기본 자료형 까지

---
+++ script/test01.js
@@ -0,0 +1,6 @@
+var msg = "hello World";
+alert(msg);
+console.log(msg);
+
+var name =prompt("이름을 입력해주세요");
+alert(name+"님 환영합니다.");
---
+++ script/test02.js
@@ -0,0 +1,7 @@
+var a = 100, b=3.14;
+var c ="안녕하세요", d= "a";
+var t = true, f=false;
+
+console.log(a +typeof(a));
+console.log(c +typeof(c));
+console.log(t +typeof(t));
