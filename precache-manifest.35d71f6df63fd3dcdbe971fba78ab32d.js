6998b48502b410a3ed4f2053995f57ca7a7ed7ca
siri_06 <siri_06@rnd.skinfosec.com>
2019-04-29 13:47:27 +0900

    Updates

---
+++ precache-manifest.35d71f6df63fd3dcdbe971fba78ab32d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "56ff55e4564928969d259f75184b538a",
+    "url": "/movie_app/index.html"
+  },
+  {
+    "revision": "0cf42113a7a33907dfc1",
+    "url": "/movie_app/static/css/main.7bcb57c4.chunk.css"
+  },
+  {
+    "revision": "9cb69e99b275b59305e2",
+    "url": "/movie_app/static/js/2.66c7f579.chunk.js"
+  },
+  {
+    "revision": "0cf42113a7a33907dfc1",
+    "url": "/movie_app/static/js/main.81e7cdf1.chunk.js"
+  },
+  {
+    "revision": "c6514544d436775abdc2",
+    "url": "/movie_app/static/js/runtime~main.175dda75.js"
+  }
+]);
