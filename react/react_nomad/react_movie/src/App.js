0ee4b97e4021cbdfb6698bc0fc7e31ab914fe5a0
siri <ysh7731@gmail.com>
2019-04-29 13:20:21 +0900

    무비앱 마지막 커밋

--- react/react_nomad/react_movie/src/App.js
+++ react/react_nomad/react_movie/src/App.js
@@ -1,63 +1,92 @@
-import React, { Component } from 'react';
-import './App.css';
-import MovieShow from './Movie';
+import React, { Component } from "react";
+import "./App.css";
+import "./Movie.css";
+import MovieShow from "./Movie";
 
-const API_KEY = 'https://yts.am/api/v2/list_movies.json?sort_by=rating';
+const API_KEY = "https://yts.am/api/v2/list_movies.json?sort_by=download_count";
 
 class App extends Component {
-    // Render : componentWillMount() -> render() -> componentDidMount()
-    // Update: componentWillReceiveProps() -> shouldComponentUpdate() -> componentWillUpdate ->
-    // render() -> componentDidUpdate()
+  // Render : componentWillMount() -> render() -> componentDidMount()
+  // Update: componentWillReceiveProps() -> shouldComponentUpdate() -> componentWillUpdate ->
+  // render() -> componentDidUpdate()
 
-    //컴포넌트 안에 state가 바뀔때마다 render를 한다.
+  //컴포넌트 안에 state가 바뀔때마다 render를 한다.
 
-    //영화 API 불러오기 이전 코드
-    // state = {}
+  //영화 API 불러오기 이전 코드
+  // state = {}
 
-    // componentDidMount(){
-    //     setTimeout(() => {
-    //         this.setState({
-    //             movies : [
-    //                 {   
-    //                     title: "헝거게임",
-    //                     poster:"https://t1.daumcdn.net/cfile/tistory/246A924F52A41A0C33"
-    //                 },
-    //                 {   
-    //                     title:"극한직업",
-    //                     poster: "https://www.rfa.org/korean/weekly_program/culture_talk/openculture-01292019140941.html/extreme_job_movie_b.jpg"
-    //                 },
-    //                 {
-    //                     title:"어벤져스",
-    //                     poster:"http://image2.megabox.co.kr/mop/poster/2019/1F/1175D3-40C3-4905-9A50-70457E37C875.large.jpg"
-    //                 }
-    //             ]
-    //         })
-    //     },3000);
-    // }
+  // componentDidMount(){
+  //     setTimeout(() => {
+  //         this.setState({
+  //             movies : [
+  //                 {
+  //                     title: "헝거게임",
+  //                     poster:"https://t1.daumcdn.net/cfile/tistory/246A924F52A41A0C33"
+  //                 },
+  //                 {
+  //                     title:"극한직업",
+  //                     poster: "https://www.rfa.org/korean/weekly_program/culture_talk/openculture-01292019140941.html/extreme_job_movie_b.jpg"
+  //                 },
+  //                 {
+  //                     title:"어벤져스",
+  //                     poster:"http://image2.megabox.co.kr/mop/poster/2019/1F/1175D3-40C3-4905-9A50-70457E37C875.large.jpg"
+  //                 }
+  //             ]
+  //         })
+  //     },3000);
+  // }
 
+  state = {};
+  // componentDidMount() {
+  //   fetch(API_KEY) // fetch로 api 내용을 가져와라
+  //     .then(res => res.json()) // 위에 작업이 끝나면 then 안에 있는 내용을 진행해라
+  //     .then(json => console.log(json)) // 위에 작업이 끝나면 then 안에 있는 내용을 진행해라
+  //     .catch(err => console.log(err)); // fetch에서 api 내용을 가져오지 못하면 error를 내라
+  //    계속해서 then 을 사용하게 되면 CallBack HELL 이 되어버림 그래서 함수들을 만들어서 작업진행
+  // }
 
-    state = {}
-    componentDidMount(){
-        fetch(API_KEY)                        // fetch로 api 내용을 가져와라 
-        .then(res =>console.log(res))// 위에 작업이 끝나면 then 안에 있는 내용을 진행해라                         
-        .catch(err => console.log(err));      // fetch에서 api 내용을 가져오지 못하면 error를 내라
-    }
+  componentDidMount() {
+    this._getMovies();
+  }
 
-    _renderMovies = () => {
-        const movies = this.state.movies.map((movie, index) => {
-            return <MovieShow key={index} title={movie.title} poster={movie.poster}/>
-        })
-        return movies;
-    }
+  _renderMovies = () => {
+    const movies = this.state.movies.map(movie => {
+      console.log(movie);
+      return (
+        <MovieShow
+          key={movie.id}
+          title={movie.title}
+          poster={movie.medium_cover_image}
+          genres={movie.genres}
+          synopsis={movie.synopsis}
+        />
+      );
+    });
+    return movies;
+  };
 
-    render(){
-        return (
-            <div className="App">
-                {this.state.movies ? this._renderMovies() : 'Loading'}
-            </div>
-            
-        )
-    }
+  _getMovies = async () => {
+    const movies = await this._callApi(); //await 내가 준 명령어가 끝나길 기다리는 것 현재 같은 경우 callApi함수가 끝나길 대기
+    this.setState({
+      movies
+    });
+  };
+
+  _callApi = () => {
+    return fetch(API_KEY) // fetch로 api 내용을 가져와라
+      .then(res => res.json()) // 위에 작업이 끝나면 then 안에 있는 내용을 진행해라
+      .then(json => json.data.movies) // 위에 작업이 끝나면 then 안에 있는 내용을 진행해라
+      .catch(err => console.log(err)); // fetch에서 api 내용을 가져오지 못하면 error를 내라
+  };
+
+  render() {
+    const { movies } = this.state;
+    return (
+      <div className={movies ? "App" : "App--loading"}>
+        {this.state.movies ? this._renderMovies() : "Loading"}
+      </div>
+    );
+  }
 }
 
 export default App;
--- react/react_nomad/react_movie/src/Movie.js
+++ react/react_nomad/react_movie/src/Movie.js
@@ -1,38 +1,62 @@
-import React, { Component } from 'react';
-import PropTypes from 'prop-types';
-// import m_css from './Movie.css';
+import React, { Component } from "react";
+import PropTypes from "prop-types";
+import LinesEllipsis from "react-lines-ellipsis";
 
-class Movie extends Component{
-    static propTypes = {
-        title: PropTypes.string.isRequired,
-        poster: PropTypes.string.isRequired,
-    }
-    render(){
-        return (
-            <div>
-                <h1>{this.props.title}</h1>
-                <MoviePoster poster={this.props.poster}/>
-            </div>
-            
-        )
-    }
+class Movie extends Component {
+  static propTypes = {
+    title: PropTypes.string.isRequired,
+    poster: PropTypes.string.isRequired,
+    genres: PropTypes.string.isRequired,
+    synopsis: PropTypes.string.isRequired
+  };
+  render() {
+    console.log(this.props.genres);
+    return (
+      <div className="Movie">
+        <div className="Movie__Columns">
+          <MoviePoster poster={this.props.poster} alt={this.props.title} />
+        </div>
+        <div className="Movie__Columns">
+          <h1>{this.props.title}</h1>
+          <div className="Movie__Genres">
+            {this.props.genres.map((genres, index) => {
+              return <MovieGenre genres={genres} key={index} />;
+            })}
+          </div>
+          <div className="Movie__synopsis">
+            <LinesEllipsis
+              text={this.props.synopsis}
+              maxLine="3"
+              ellipsis="..."
+              trimRight
+              basedOn="letters"
+            />
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
 
-class MoviePoster extends Component{
-    
-
-    static propTypes = {
-        poster: PropTypes.string.isRequired,
-    }
-    render(){
-        return(
-            <div>
-                <img src={this.props.poster} alt=""/>
-            </div>
-        )
-    }
+class MoviePoster extends Component {
+  static propTypes = {
+    poster: PropTypes.string.isRequired
+  };
+  render() {
+    return (
+      <div>
+        <img src={this.props.poster} alt={this.props.alt} />
+      </div>
+    );
+  }
 }
 
+function MovieGenre({ genres }) {
+  return <span className="Movie__Genre">{genres}</span>;
+}
 
+MovieGenre.propTypes = {
+  genres: PropTypes.string.isRequired
+};
 
 export default Movie;
